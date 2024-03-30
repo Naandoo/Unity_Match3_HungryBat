47c0ee1eca144e3145eb142713892659085f2701
Nando <Fernandocelso25@gmail.com>
2024-03-30 10:53:16 -0300

    [ADD] Skill UI integrated with system and fixes in skill behaviour

--- HungryBat/Assets/Game/Scripts/Skills/Skill.cs
+++ HungryBat/Assets/Game/Scripts/Skills/Skill.cs
@@ -6,6 +6,7 @@ namespace Skill
 {
     public abstract class Skill : ScriptableObject
     {
+        public string Name;
         public int CurrentAmount { get; set; } // TODO: Add skills based on some system / level design
         public string Description;
         public abstract IEnumerator Execute(Fruit selectedFruit, Fruit[,] boardFruit);
--- HungryBat/Assets/Game/Scripts/Skills/SkillManager.cs
+++ HungryBat/Assets/Game/Scripts/Skills/SkillManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using Board;
 using FruitItem;
+using TMPro;
 using UnityEngine;
 
 namespace Skill
@@ -11,6 +12,8 @@ namespace Skill
         [SerializeField] private BoardMatcher _boardMatcher;
         [SerializeField] private BoardSorter _boardSorter;
         [SerializeField] private Canvas _skillUI;
+        [SerializeField] private TMP_Text _skillNameUI;
+        [SerializeField] private TMP_Text _skillDescriptionUI;
         private bool skillState;
         private Skill selectedSkill = null;
 
@@ -18,22 +21,36 @@ namespace Skill
         {
             BoardState.Instance.SetState(State.WaitingAction);
             skillState = true;
-            // _skillUI.enabled = true;
+            UpdateSkillUI(selectedSkill: skill);
             selectedSkill = skill;
         }
 
         public IEnumerator CheckSkillState(Fruit fruit)
         {
             if (!skillState) yield break;
-            // _skillUI.enabled = false;
+            _skillUI.enabled = false;
+
+            skillState = false;
 
             yield return StartCoroutine(selectedSkill.Execute(fruit, _boardGrid.BoardFruitArray));
 
-            skillState = false;
             selectedSkill = null;
 
             StartCoroutine(_boardSorter.SortBoard());
             _boardMatcher.TryMatchFruits(matchWithMovement: false);
+        }
+
+        private void UpdateSkillUI(Skill selectedSkill)
+        {
+            _skillUI.enabled = true;
+            _skillNameUI.text = selectedSkill.Name;
+            _skillDescriptionUI.text = selectedSkill.Description;
+        }
+        public void CancelSkillUse()
+        {
+            _skillUI.enabled = false;
+            skillState = false;
+            selectedSkill = null;
         }
 
     }
