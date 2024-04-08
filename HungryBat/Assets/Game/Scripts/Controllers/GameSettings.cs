7c5dd798a6576160e3f58071a2a772eec0253bf9
Nando <Fernandocelso25@gmail.com>
2024-04-08 11:28:43 -0300

    [ADD] Settings and a few fixes in code

---
+++ HungryBat/Assets/Game/Scripts/Controllers/GameSettings.cs
@@ -0,0 +1,25 @@
+using Game.UI;
+using ScriptableVariables;
+using UnityEngine;
+
+namespace Controllers
+{
+    public class GameSettings : MonoBehaviour
+    {
+        [SerializeField] private BoolVariable _soundAvailable;
+        [SerializeField] private UiAnimation _uiAnimation;
+        [SerializeField] private BoolVariable _paused;
+        [SerializeField] private Canvas _pauseScreen;
+
+        public void TriggerPause()
+        {
+            _paused.Value = !_paused.Value;
+
+            Time.timeScale = _paused.Value ? 0 : 1;
+            _pauseScreen.enabled = _paused.Value;
+            AudioListener.pause = _paused.Value;
+        }
+
+        public void TriggerSound() => _soundAvailable.Value = !_soundAvailable.Value;
+    }
+}
